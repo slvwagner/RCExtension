d5dad074bf85478ff912c1c3e3d1a83301d77de1
Florian Wagner <53572950+slvwagner@users.noreply.github.com>
2021-08-20 13:38:58 +0200

    minor inprogramm documentation update: Flugbahn

--- src/MyRCExtn1.c
+++ src/MyRCExtn1.c
@@ -158,11 +158,12 @@ SEXP Flugbahn (SEXP v0, SEXP t ,SEXP angle_Schussebenen, SEXP Ziel_Schussebenen,
       // Abbruch wenn der Flugwinkel fast -90 Grad wird als0 das Geschoss nur noch fällt.
       // Abbruch wenn for loop fertig
       if (dist_ < betrag(REAL(Ziel_Schussebenen)[0] - p_sx[i], REAL(Ziel_Schussebenen)[1] - p_sy[i]) || round(angle_ / pi * 180 * 1000) / 1000 < -89.99 || i == iter-1) {
-
+        // calculate index to get values from pointers
         int ratio = floor(i / c_nb_values-1);
         for (int i = 0; i < (c_nb_values) ; i++) {
           c_seq[i] = i * ratio;
         }
+        // correct last value
         c_seq[c_nb_values-1] = i;
 
         //apply data to pointer
